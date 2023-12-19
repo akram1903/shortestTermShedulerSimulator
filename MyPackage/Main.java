c510a6f9534f3092584604576d8c8fac0b8d70d9
Akram Essam <akram.a.e.2002@gmail.com>
2023-12-19 21:28:51 +0200

    final final

--- MyPackage/Main.java
+++ MyPackage/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         GenerateProcesses g = new GenerateProcesses();
-        ArrayList<MyProcess> arr = g.generate(3,10);
+        ArrayList<MyProcess> arr = g.generate(5,30);
         // ArrayList<MyProcess> arr = new ArrayList<>();
 
         STS shortestTermScheduler = new STS(arr);
